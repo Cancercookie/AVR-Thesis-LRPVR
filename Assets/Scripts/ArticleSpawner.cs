1a9333cd43ffc48da7c5acdf6aadd218da5aca02
Cancercookie <jaritarabelli1@icloud.com>
2019-04-01 18:47:13 +0200

    MR-01
    Done some basic spawn scripting
    Updated to 2018.3.11
    Removed multiple shelf

---
+++ Assets/Scripts/ArticleSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArticleSpawner : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 1; i < 5; i++){
+           Instantiate(prefab, new Vector3(prefab.transform.position.x, prefab.transform.position.y, prefab.transform.position.z + i * 0.5f), Quaternion.identity);
+        }  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
