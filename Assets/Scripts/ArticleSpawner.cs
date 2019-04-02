3bf48368fabdf22fe188cc97d9993e1085757627
Cancercookie <jaritarabelli1@icloud.com>
2019-04-02 18:31:53 +0200

    MR-01
    Spawn (wip)

--- Assets/Scripts/ArticleSpawner.cs
+++ Assets/Scripts/ArticleSpawner.cs
@@ -5,14 +5,39 @@ using UnityEngine;
 public class ArticleSpawner : MonoBehaviour
 {
     [SerializeField]
-    public GameObject prefab;
+    private GameObject[] gameObjects;
+    [SerializeField]
+    private char[] directions;
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 1; i < 5; i++){
-           Instantiate(prefab, new Vector3(prefab.transform.position.x, prefab.transform.position.y, prefab.transform.position.z + i * 0.5f), Quaternion.identity);
-        }  
+        for(var i = 0; i < gameObjects.Length; i++)
+        {
+            var prefab = gameObjects[i];
+            var prefabX = 0f;
+            var prefabY = 0f;
+            var prefabZ = 0f;
+            if(directions[i] == 'x')
+            {
+                prefabX = 1;
+            }else if(directions[i] == 'y')
+            {
+                prefabY = 1;
+            }
+            else if(directions[i] == 'z')
+            {
+                prefabZ = 1;
+            }
+            for (int j = 1; j <=10; j++)
+            {
+                prefabX = prefabX * j * 0.2f;
+                prefabY = prefabY * j * 0.2f;
+                prefabZ = prefabZ * j * 0.2f;
+                Instantiate(prefab, new Vector3(prefab.transform.position.x + prefabX, prefab.transform.position.y + prefabY, prefab.transform.position.z + prefabZ), transform.rotation * Quaternion.Euler(0f, 0f, 90f));
+            }
+        }
+        
     }
 
     // Update is called once per frame
