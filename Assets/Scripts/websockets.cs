2c4928345a4a03fc3f7d1c5afd2ed173fc408002
Cancercookie <jaritarabelli1@icloud.com>
2019-04-17 18:24:36 +0200

    FAC-01
    Started working with websockets

---
+++ Assets/Scripts/websockets.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+
+public class websockets : MonoBehaviour
+{
+    public WebSocket ws = new WebSocket("wss://cxr4c7tqeh.execute-api.eu-west-1.amazonaws.com/production");
+    // Start is called before the first frame update
+    void Start()
+    {
+        ws.OnOpen += (sender, e) => {
+            Debug.Log("connection");
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
