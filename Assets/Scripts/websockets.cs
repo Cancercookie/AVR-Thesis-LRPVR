6751bafa378889752b0b1d548afae7024bd9bfa7
cancerookie <jaritarabelli1@icloud.com>
2019-04-18 21:39:13 +0200

    FAC-01
    FIrst websocket connection
    must remove some garbage

--- Assets/Scripts/websockets.cs
+++ Assets/Scripts/websockets.cs
@@ -1,17 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Net.WebSockets;
 using UnityEngine;
-using WebSocketSharp;
 
 public class websockets : MonoBehaviour
 {
-    public WebSocket ws = new WebSocket("wss://cxr4c7tqeh.execute-api.eu-west-1.amazonaws.com/production");
+    readonly Uri u = new Uri("wss://cxr4c7tqeh.execute-api.eu-west-1.amazonaws.com/production");
+    ClientWebSocket cws = null;
+    ArraySegment<byte> buf = new ArraySegment<byte>(new byte[1024]);
+
     // Start is called before the first frame update
     void Start()
     {
-        ws.OnOpen += (sender, e) => {
-            Debug.Log("connection");
-        };
+        Connect();
+    }
+
+    async void Connect()
+    {
+        cws = new ClientWebSocket();
+        try
+        {
+            await cws.ConnectAsync(u, CancellationToken.None);
+            if (cws.State == WebSocketState.Open) Debug.Log("connected");
+            WSRead();
+            WSReceiver();
+        }
+        catch (Exception e) { Debug.Log("woe " + e.Message); }
+    }
+
+    async void WSRead()
+    {
+        ArraySegment<byte> b = new ArraySegment<byte>(Encoding.UTF8.GetBytes("{action: read}"));
+        await cws.SendAsync(b, WebSocketMessageType.Text, true, CancellationToken.None);
+    }
+
+    
+    async void WSReceiver()
+    {
+        WebSocketReceiveResult r = await cws.ReceiveAsync(buf, CancellationToken.None);
+        Debug.Log("Got: " + Encoding.UTF8.GetString(buf.Array, 0, r.Count));
+        WSReceiver();
     }
 
     // Update is called once per frame
