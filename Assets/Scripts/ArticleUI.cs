aa149bb6c88a79e6b4d07e8e35ea238a9d740f43
cancerookie <jaritarabelli1@icloud.com>
2019-05-04 01:55:00 +0200

    FAC-01
    almost done
    last changes needed
    see in the note book
    (ready to integrate AWS)
    PS. Updated to 2019.1.1f1
    PPS. Removed HTC Plugin thinking it was generating conflict for TP

--- Assets/Scripts/ArticleUI.cs
+++ Assets/Scripts/ArticleUI.cs
@@ -1,23 +1,45 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using Valve.VR;
 
 public class ArticleUI : MonoBehaviour
 {
     private float speed = 50.0f;
     public Article article;
-    public bool visible = false;
-    private string URI = "ArticleUI/Canvas";
+    private GameObject player;
+
+    private bool visible = false;
+    private bool opened = false;
+    [SerializeField]
+    private GameObject AVR_Canvas;
+    [SerializeField]
+    private Camera UI_Camera;
     private Canvas canvas;
+    private SteamVR_Action_Boolean interactUI;
+    private ScrollRect infoScroll;
+
+    private void Awake()
+    {
+        interactUI = SteamVR_Actions._default.InteractUI;
+    }
 
     void Start()
     {
         canvas = this.GetComponentInChildren<Canvas>();
+        infoScroll = AVR_Canvas.GetComponentInChildren<ScrollRect>();
+        infoScroll.verticalScrollbarVisibility = ScrollRect.ScrollbarVisibility.AutoHide;
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     void Update()
     {
         canvas.gameObject.SetActive(visible);
+        AVR_Canvas.SetActive(visible);
         select();
+        if (visible)
+        {
+            transform.LookAt(player.transform);
+        }
     }
 
     public void open()
@@ -25,34 +47,22 @@ public class ArticleUI : MonoBehaviour
         visible = true;
         articlePrep();
         UIPrep();
-    }
-
-    private void Spin()
-    {
-        article.gameObject.transform.Rotate(Vector3.right, speed * Time.deltaTime);
+        opened = true;
     }
 
     private void articlePrep()
     {
-        //Vector3 scale = article.gameObject.transform.localScale;
-        //scale.x = scale.x * 0.5f;
-        //scale.y = scale.y * 0.5f;
-        //scale.z = scale.z * 0.5f; ;
-        //article.gameObject.transform.localScale = scale;
-        //article.gameObject.GetComponent<Rigidbody>().useGravity = false;
+        // LOCK IN PLACE
+        // KEEP HIGHLIGHTED
     }
 
     private void UIPrep()
     {
-
-        // MOVE UI TO ARTICLE LOCATION + OFFSET
         transform.position = article.transform.position;
-        Text description = GameObject.FindWithTag("Description").GetComponent<Text>();
+        Text description = infoScroll.GetComponentInChildren<Text>();
         Text price = GameObject.FindWithTag("Price").GetComponent<Text>();
         Text title = GameObject.FindWithTag("Title").GetComponent<Text>();
-        ScrollRect scroll = this.GetComponentInChildren<ScrollRect>();
 
-        scroll.verticalScrollbarVisibility = ScrollRect.ScrollbarVisibility.AutoHide;
         title.text = article.articleName;
         price.text = article.price.ToString("F") + "€";
         description.text = article.description;
@@ -61,24 +71,37 @@ public class ArticleUI : MonoBehaviour
     private void select()
     {
         RaycastHit hit;
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 300f))
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 500f) && interactUI.GetStateDown(SteamVR_Input_Sources.Any) && !opened)
         {
-            Debug.Log(hit.transform.name);
             article = hit.transform.GetComponent<Article>();
             if (article != null)
-            {
+            { 
                 open();
             }
             else
             {
-                visible = false;
+                close();
             }
-            
         }
+        else if (Physics.Raycast(UI_Camera.transform.position, Camera.main.transform.forward, out hit, 500f) && interactUI.GetStateUp(SteamVR_Input_Sources.Any) && opened)
+        {
+            if (hit.transform.GetComponent<cartHandler>())
+            {
+                addToCart();
+            }
+            close();
+        }
+    }
+
+    public void addToCart()
+    {
+        
     }
 
     public void close()
     {
         visible = false;
+        opened = false;
     }
 }
+
